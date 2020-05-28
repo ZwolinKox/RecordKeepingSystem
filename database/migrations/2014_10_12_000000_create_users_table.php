86b5f3e4fb9e33d1a1ce21293ff2749a9946a617
Mateusz Sarnacki <mateks301@interia.pl>
2020-05-28 14:32:32 +0200

    Naprawa columny admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateUsersTable extends Migration
 {
@@ -19,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('admin');
+            $table->boolean('admin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
