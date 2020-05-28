8f00e759f63196e1fef932e1718da1725ecab0c6
Szymon Zwoliński <zwolinn@wp.pl>
2020-05-28 17:28:31 +0200

    Dodanie do API pozyskiwania informacji o uzytkownikach

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\User;
+
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
 Route::get('logout', 'AuthController@logout');
 Route::get('user', 'AuthController@getAuthUser'); //Wysyła json z danymi o użytkowniku na podstawie jwt
+
+
+Route::get('users/', 'UsersController@getUsers');
+Route::get('users/{id}', 'UsersController@getUser');
