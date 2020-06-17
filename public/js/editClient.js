0e52e703a5a642a2cfcd063fd61493a73b68d25f
Jakub Piszczan <jpiszczan@gmail.com>
2020-06-17 02:24:11 +0200

    edycja klienta -grupy

--- public/js/editClient.js
+++ public/js/editClient.js
@@ -38,13 +38,120 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.querySelector("#eclient_sms").checked = false;
 
             document.querySelector("#eclient_name").value = values.name;
-            document.querySelector("#eclient_group").value = values.name;
             document.querySelector("#eclient_tnum").value = values.phone1;
             document.querySelector("#eclient_atnum").value = values.phone2;
             document.querySelector("#eclient_dea").value = values.email1;
             document.querySelector("#eclient_aea").value = values.email2;
 
+            fetch("/api/clients/groups/" + getParam(),
+            {
+                method: "get",
+                headers:
+                {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + Cookies.get("token")
+                },
+            })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return;
+                }
+            })
+            .then(res => {
+                values = JSON.parse(JSON.stringify(res));
+
+                values.forEach(element => {
+                    document.querySelector("#eclient_deleteGroup").innerHTML +=
+                    `<option value="${element.id}">${element.name}</option>`
+                })
+
+                document.querySelector("#eclient_deleteGroupButton").addEventListener("click", () =>
+                 {
+                                                    console.log(document.querySelector("#eclient_deleteGroup").value);
+                                                    const ob_ng = {
+                                                        group: document.querySelector("#eclient_deleteGroup").value, 
+                                                        client: getParam()
+                                                    }
+
+                                                    fetch("/api/groups/disconnect",
+                                                    {
+                                                        method: "post",
+                                                        headers:
+                                                        {
+                                                            "Content-Type": "application/json",
+                                                            "Accept": "application/json",
+                                                            "Authorization": "Bearer " + Cookies.get("token")
+                                                        },
+                                                        body: JSON.stringify(ob_ng)
+                                                    }).then(res => {
+                                                        if(res.ok)
+                                                            location.reload();
+                                                        else {
+                                                            alert("Wielbłąd");
+                                                        }
+                                                    })
+                })
+            })
+
             
+
+            fetch("/api/groups",
+            {
+
+                method: "get",
+                headers:
+                {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + Cookies.get("token")
+                },
+
+            })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return;
+                }
+            })
+            .then(res => {
+                values = JSON.parse(JSON.stringify(res));
+                
+                values.data.forEach(element => {
+                    document.querySelector("#eclient_addgroup").innerHTML +=
+                    `<option value="${element.id}">${element.name}</option>`
+                })
+
+                document.querySelector("#eclient_addGroupButton").addEventListener("click", () =>
+                 {
+                                                    console.log(document.querySelector("#eclient_addgroup").value);
+                                                    const ob_ng = {
+                                                        group: document.querySelector("#eclient_addgroup").value, 
+                                                        client: getParam()
+                                                    }
+
+                                                    fetch("/api/groups/connect",
+                                                    {
+                                                        method: "post",
+                                                        headers:
+                                                        {
+                                                            "Content-Type": "application/json",
+                                                            "Accept": "application/json",
+                                                            "Authorization": "Bearer " + Cookies.get("token")
+                                                        },
+                                                        body: JSON.stringify(ob_ng)
+                                                    }).then(res => {
+                                                        if(res.ok)
+                                                            location.reload();
+                                                        else {
+                                                            alert("Wielbłąd");
+                                                        }
+                                                    })
+                })
+            })
             
 
             $("#loading").fadeOut("slow", function () {
